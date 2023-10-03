7bac5465fc3dd8b6c60af4496392282720c4a461
Temitope.Aniyikaye <aniyikayetemitopeflorence@gmail.com>
2023-10-03 17:18:47 +0100

    API-gateway service

---
+++ api-gateway/src/test/java/asherflo/com/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package asherflo.com.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- employee-service/src/main/java/com/asherflo/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/asherflo/employeeservice/EmployeeServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 @EnableFeignClients
 @SpringBootApplication
+
 public class EmployeeServiceApplication {
 
 	public static void main(String[] args) {
